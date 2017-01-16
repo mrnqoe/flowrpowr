d3f4234a6ec003651a048e2daabc41ca555ce93b
Nicolas Moreau <mrnqoe@gmail.com>
2017-01-16 21:24:00 +0000

    This is cool

---
+++ app.js
@@ -0,0 +1,16 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+function futureGen(){
+	request.get('https://sytantris.github.io/http-examples/future.jpg')
+	      .on('error', function (err) {
+	        console.log(err);
+	      })
+	      .on('response', function (response) {
+	        console.log('Response Status Code: ', response.statusCode);
+					console.log('Response Content Type: ', response.headers['content-type']);
+	      })
+	      .pipe(fs.createWriteStream('./future.jpg'));
+}
+
+futureGen();
